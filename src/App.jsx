d289fc783f9bbbde8942cc86e21be3ec4e40b5f1
VishalOz <vishal.20240036@iit.ac.lk>
2025-06-10 00:27:05 +0530

    Carousel structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import Carousel from './Carousel'
 
 const App = () => {
-  const image = [
-    img1.jpg,
-    img2.jpg,
-    img3.jpg,
-    img4.jpg,
-    img5.jpg
+  const images = [
+    "./assets/img1.jpg",
+    "./assets/img2.jpg",
+    "./assets/img3.jpg",
+    "./assets/img4.jpg",
+    "./assets/img5.jpg"
   ];
   
   return (
     <div className="p-4">
       <h1 className="text-3xl text-center font-bold mb-4">Image Carousel</h1>
-      <Carousel images={image} />
+      <Carousel images={images} />
     </div>
   );
 }
