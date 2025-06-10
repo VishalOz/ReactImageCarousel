e7b3f9cc827aad7e29d15bdd90c0558678be495c
VishalOz <vishal.20240036@iit.ac.lk>
2025-06-10 08:44:16 +0530

    Carousel removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import React from 'react'
-import Carousel from './Carousel'
 
 const App = () => {
-  const images = [
-    "./assets/img1.jpg",
-    "./assets/img2.jpg",
-    "./assets/img3.jpg",
-    "./assets/img4.jpg",
-    "./assets/img5.jpg"
-  ];
-  
   return (
-    <div className="p-4">
-      <h1 className="text-3xl text-center font-bold mb-4">Image Carousel</h1>
-      <Carousel images={images} />
+    <div>
+      
     </div>
-  );
+  )
 }
 
 export default App
